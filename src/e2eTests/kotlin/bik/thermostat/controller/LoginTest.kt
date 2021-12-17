929b23691925e686bea0a7de277a9c1349126d91
inigodm <inigo.delgado@gmail.com>
2021-12-17 01:06:34 +0100

    Create a HTTPUtil class

--- src/e2eTests/kotlin/bik/thermostat/controller/LoginTest.kt
+++ src/e2eTests/kotlin/bik/thermostat/controller/LoginTest.kt
@@ -1,5 +1,6 @@
 package bik.thermostat.controller
 
+import bik.thermostat.utils.HttpUtil
 import http.Petition
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -8,19 +9,31 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LoginTest {
     val URL = "http://localhost:8080/user"
-    val URL_LOGIN = "http://localhost:8080/login"
     val URL_LOGOUT = "http://localhost:8080/login"
-    val APPLICATION_JSON = "application/json"
-    val data = "{\"username\":\"user\", \"password\":\"pass\"}"
     val deleteData = "{\"username\":\"user\"}"
+    val URL_SCHEDULE = "schedules"
+
+    companion object {
+        val URL_LOGIN = "http://localhost:8080/login"
+        val data = "{\"username\":\"user\", \"password\":\"pass\"}"
+
+        @JvmStatic fun makeLoggedPetitionTo(url: String) : Petition{
+            val response = Petition.to(URL_LOGIN)
+                .withContentType(HttpUtil.APPLICATION_JSON)
+                .sendAPost(this.data)
+            return Petition.to(url)
+                .withContentType(HttpUtil.APPLICATION_JSON)
+                .withHeader("Authorization", response.headers("Authorization")!!)
+        }
+    }
 
     @BeforeEach
     fun setup() {
         Petition.to(URL)
-            .withContentType(APPLICATION_JSON)
+            .withContentType(HttpUtil.APPLICATION_JSON)
             .sendADelete(deleteData)
         Petition.to(URL)
-            .withContentType(APPLICATION_JSON)
+            .withContentType(HttpUtil.APPLICATION_JSON)
             .sendAPost(data)
     }
 
@@ -28,7 +41,7 @@ class LoginTest {
     fun should_login_user() {
         //https://blog.softtek.com/es/autenticando-apis-con-spring-y-jwt
         val response = Petition.to(URL_LOGIN)
-            .withContentType(APPLICATION_JSON)
+            .withContentType(HttpUtil.APPLICATION_JSON)
             .sendAPost(data)
 
         response.assertThatResponseIsOk()
@@ -39,19 +52,28 @@ class LoginTest {
     fun should_logout_user() {
         //https://blog.softtek.com/es/autenticando-apis-con-spring-y-jwt
         val response = Petition.to(URL_LOGIN)
-            .withContentType(APPLICATION_JSON)
+            .withContentType(HttpUtil.APPLICATION_JSON)
             .sendAPost(data)
+
         Petition.to(URL_LOGOUT)
-            .withContentType(APPLICATION_JSON)
+            .withContentType(HttpUtil.APPLICATION_JSON)
             .withHeader("Authorization", response.headers("Authorization")!!)
-            .sendAPost(data)
+            .sendAGet()
             .assertThatResponseIsOk()
+
+        Petition.to(URL_SCHEDULE)
+            .withContentType(HttpUtil.APPLICATION_JSON)
+            .withHeader("Authorization", response.headers("Authorization")!!)
+            .sendAGet()
+            .assertThatResponseCodeIs(401)
     }
 
     @Test
-    fun should_response_with_a_403_if_has_not_bearer() {
+    fun should_response_with_a_403_if_not_logged() {
         //https://blog.softtek.com/es/autenticando-apis-con-spring-y-jwt
-        //Petition.sendAGetTo(URL)
-        //    .assertThatResponseCodeIs(401)
+        Petition.to(URL_SCHEDULE)
+            .withContentType(HttpUtil.APPLICATION_JSON)
+            .sendAGet()
+            .assertThatResponseCodeIs(401)
     }
 }
---
+++ src/e2eTests/kotlin/bik/thermostat/utils/HttpUtil.kt
@@ -0,0 +1,7 @@
+package bik.thermostat.utils
+
+class HttpUtil {
+    companion object {
+        val APPLICATION_JSON = "application/json"
+    }
+}
